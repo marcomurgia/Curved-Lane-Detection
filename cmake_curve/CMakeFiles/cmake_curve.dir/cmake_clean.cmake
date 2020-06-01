cce62612da630833febc9f2b323ffecec95ec764
Marco_Murgia <marcomurgia9112@gmail.com>
2020-06-01 11:37:07 +0200

    file dopo cmake e make

---
+++ cmake_curve/CMakeFiles/cmake_curve.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_curve.dir/curve_lane_detection.cpp.o"
+  "cmake_curve.pdb"
+  "cmake_curve"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_curve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
